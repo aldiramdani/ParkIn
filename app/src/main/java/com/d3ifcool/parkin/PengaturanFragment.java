56b69059ad9e7910cee8b870612e2ea71103d8b4
AldiRamdani <aldiramdani8@gmail.com>
2018-03-04 22:08:15 +0700

    V 0.1

--- app/src/main/java/com/d3ifcool/parkin/PengaturanFragment.java
+++ app/src/main/java/com/d3ifcool/parkin/PengaturanFragment.java
@@ -1,18 +1,22 @@
 package com.d3ifcool.parkin;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PengaturanFragment extends Fragment {
-
+    ImageView profPrev;
 
     public PengaturanFragment() {
         // Required empty public constructor
@@ -23,7 +27,22 @@ public class PengaturanFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pengaturan, container, false);
+        View v = inflater.inflate(R.layout.fragment_pengaturan,container,false);
+        profPrev = (ImageView)v.findViewById(R.id.profPrev);
+        profPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i,0);
+            }
+        });
+        return v;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        profPrev.setImageBitmap(bitmap);
+    }
 }
