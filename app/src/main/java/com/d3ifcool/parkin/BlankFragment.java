7aa9afde893b17c8da43999a3cb1cff616037451
AldiRamdani <aldiramdani8@gmail.com>
2018-03-11 19:35:27 +0700

    V 0.1

--- app/src/main/java/com/d3ifcool/parkin/BlankFragment.java
+++ app/src/main/java/com/d3ifcool/parkin/BlankFragment.java
@@ -1,6 +1,8 @@
 package com.d3ifcool.parkin;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
 /**
@@ -20,7 +23,7 @@ import android.widget.ImageView;
  */
 public class BlankFragment extends Fragment {
     ImageButton floatbtn;
-
+    Button btn_hapus1;
     public BlankFragment() {
         // Required empty public constructor
     }
@@ -29,9 +32,11 @@ public class BlankFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_blank, container, false);
         floatbtn = (ImageButton)v.findViewById(R.id.test);
+        btn_hapus1 = (Button)v.findViewById(R.id.btn_hapus1);
         final BlankFragment blankFragment = new BlankFragment();
         floatbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,8 +44,21 @@ public class BlankFragment extends Fragment {
             ChangeFragment();
             }
         });
-        return v;
 
+        
+        btn_hapus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setMessage("Yakin Akan Di Hapus?")
+                    .setPositiveButton("ok", null)
+                    .setNegativeButton("Cancel",null);
+            AlertDialog alert = builder.create();
+            alert.show();
+            }
+        });
+
+        return v;
     }
     private void ChangeFragment(){
     getFragmentManager().beginTransaction().replace(R.id.frame_parkir,new MainFragment()).addToBackStack(null).commit();
--- app/src/main/java/com/d3ifcool/parkin/MainActivity.java
+++ app/src/main/java/com/d3ifcool/parkin/MainActivity.java
@@ -9,8 +9,10 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -18,17 +20,30 @@ public class MainActivity extends AppCompatActivity {
     ///Test Push
     ImageView coba;
     TextView txt_daftar;
+    EditText username_login;
+    EditText password_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //inisiasi
         coba = (ImageView)findViewById(R.id.coba);
         txt_daftar = (TextView)findViewById(R.id.txt_daftar);
+        username_login = (EditText)findViewById(R.id.username_login);
+        password_login = (EditText)findViewById(R.id.password_login);
+        //
         coba.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String username = username_login.getText().toString();
+                String password = password_login.getText().toString();
+                if (username.equals("admin") & password.equals("admin")){
                 Intent i = new Intent(getApplicationContext(),BerandaActivity.class);
                 startActivity(i);
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(),"Username/Password Salah!",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         });
         txt_daftar.setOnClickListener(new View.OnClickListener() {
